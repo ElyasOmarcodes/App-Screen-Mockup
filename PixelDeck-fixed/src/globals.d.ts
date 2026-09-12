1e4c833079f26656eb08d501d2f6aea8c8942a2e
ElyasOmarcodes <ElyasOmar.001@gmail.com>
2026-09-12 20:30:09 +0430

    Delete PixelDeck-fixed directory

--- PixelDeck-fixed/src/globals.d.ts
+++
@@ -1,3 +0,0 @@
-// Build-time constants injected by vite.config.ts → define
-declare const __APP_VERSION__: string
-declare const __GIT_HASH__: string
--- PixelDeck-fixed/src/store/storage/index.ts
+++
@@ -1,9 +0,0 @@
-import { getPixelDeckConfig } from '@/config'
-import { localStorageAdapter } from './localStorageAdapter'
-import type { ProjectStorageAdapter } from './types'
-
-export function getProjectStorage(): ProjectStorageAdapter {
-  return getPixelDeckConfig().projectStorage ?? localStorageAdapter
-}
-
-export * from './types'
